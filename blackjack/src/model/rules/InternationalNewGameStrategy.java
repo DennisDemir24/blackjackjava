bcba6d21a8bb1281e223e16a6b7afc9f7217fcac
Patrik-Hasselbad <phasselblad@hotmail.com>
2020-10-28 21:49:31 +0100

    Cleaned up some bad code

--- blackjack/src/model/rules/InternationalNewGameStrategy.java
+++ blackjack/src/model/rules/InternationalNewGameStrategy.java
@@ -1,6 +1,5 @@
 package model.rules;
 
-import model.Card;
 import model.Dealer;
 import model.Deck;
 import model.Player;
@@ -8,8 +7,6 @@ import model.Player;
 class InternationalNewGameStrategy implements INewGameStrategy {
 
     public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-        Card c;
-
         a_player.DealCard(a_deck.GetCard(), true);
         a_dealer.DealCard(a_deck.GetCard(), true);
         a_player.DealCard(a_deck.GetCard(), true);
